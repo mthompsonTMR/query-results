8ea5ad75eaa1992be98d2f0b70170bf53a1087e4
getdevopsready <mthompson_tmr@hotmail.com>
2025-04-14 10:35:13 -0700

    Add query and result files for playstore and products databases

---
+++ products_queries.sql
@@ -0,0 +1,55 @@
+-- 1. Add a product: chair
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44.00, FALSE);
+
+-- 2. Add a product: stool
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, TRUE);
+
+-- 3. Add a product: table
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, FALSE);
+
+-- 4. Display all rows and columns
+
+SELECT * FROM products;
+
+-- 5. Display all product names
+
+SELECT name FROM products;
+
+-- 6. Display all product names and prices
+
+SELECT name, price FROM products;
+
+-- 7. Add a new product: bookshelf
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('bookshelf', 75.00, TRUE);
+
+-- 8. Display only products that can be returned
+
+SELECT * FROM products WHERE can_be_returned = TRUE;
+
+-- 9. Display products with price < 44.00
+
+SELECT * FROM products WHERE price < 44.00;
+
+-- 10. Display products with price between 22.50 and 99.99
+
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+
+-- 11. Sale: Subtract $20 from all prices
+
+UPDATE products SET price = price - 20;
+
+-- 12. Remove products with price < 25
+
+DELETE FROM products WHERE price < 25;
+
+-- 13. End of sale: Add $20 back to all remaining products
+
+UPDATE products SET price = price + 20;
+
+-- 14. New policy: All products are returnable
+
+UPDATE products SET can_be_returned = TRUE;
